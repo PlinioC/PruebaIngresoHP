619ab7e55e3502ce0b8d0b28c160027b0d2330b9
Plinio Alejandro Carrasquero Patiño <plinio.carrasquero@gmail.com>
2019-10-27 18:02:30 -0500

    se suben cambios en pruebas unitarias

--- Api_Rest_NodeJS/src/UnitTest.js
+++ Api_Rest_NodeJS/src/UnitTest.js
@@ -2,9 +2,15 @@ const assert = require('chai').assert;
 const app = require('./rutas/index');
 
 describe('App', function(){
- it ('La aplicacion debe retornar error 442', function (){
-     var NumberEntrada = [1,2,3];
-     assert.equal(app.Crear_Arreglo(NumberEntrada),NumberEntrada);
+ it ('La aplicacion debe retornar exitoso y devolver el jsoncompleto', function (){
+    
+     assert.equal(app.Test_Metodo(NumberEntrada),);
+    });
  });
 
+ describe('App', function(){
+    it ('La aplicacion debe retornar el arreglo en arry ', function (){
+        var NumberEntrada = "123";
+        assert.equal(app.Crear_Arreglo(NumberEntrada),NumberEntrada);
+    });
 });
